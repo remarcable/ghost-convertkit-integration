a189ab28d9bacea76c523b391cacced0360cfb88
Marc N <git@marcnitzsche.de>
2024-01-12 12:45:55 +0000

    Add boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+// Things to create:
+// Sync subscribes convertkit => ghost
+// Sync subscribes ghost => convertkit
+// Sync unsubscribes convertkit => ghost
+// Sync unsubscribes ghost => convertkit
+
+// nice to have:
+// Sync tags from convertkit to ghost (and vice versa)
+// => needed to know when a sequence was completed
+// also in ghost: what happens when user resubscribe?
+// also in ck: what happens when users sign up multiple times or for multiple forms?
+
+// current deployment: https://233zx64y80.execute-api.us-east-1.amazonaws.com/
+
+// need a simple script to setup the integration, i.e. create webhooks both in ghost and ck
+
+// Most important things:
+// subscribe on ck => ghost
+// unsubscibre ck => ghost
+// un/sub ghost => ck
+
 module.exports.handler = async (event) => {
   return {
     statusCode: 200,
@@ -11,3 +32,12 @@ module.exports.handler = async (event) => {
     ),
   };
 };
+
+const createGhostMember = () => {};
+const createConvertkitMember = () => {};
+
+const unsubscribeGhostMember = () => {};
+const unsubscribeConvertkitMember = () => {};
+
+const addTagToGhostSubscriber = () => {};
+const removeTagFromGhostSubscriber = () => {};
