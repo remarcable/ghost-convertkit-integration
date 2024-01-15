baf8e88b9ea5b770fd5f9dd80056bdac811b6068
Marc N <git@marcnitzsche.de>
2024-01-15 15:29:17 +0000

    Remove todo scripts

--- src/index.mjs
+++ src/index.mjs
@@ -1,25 +1,3 @@
-// Things to create:
-// Sync subscribes convertkit => ghost
-// Sync subscribes ghost => convertkit
-// Sync unsubscribes convertkit => ghost
-// Sync unsubscribes ghost => convertkit
-
-// nice to have:
-// Sync tags from convertkit to ghost (and vice versa)
-// => needed to know when a sequence was completed
-// also in ghost: what happens when user resubscribe?
-// also in ck: what happens when users sign up multiple times or for multiple forms?
-
-// TODO: following URL is now in git. Might need a secret for the webhooks
-// current deployment: https://233zx64y80.execute-api.us-east-1.amazonaws.com/
-
-// need a simple script to setup the integration, i.e. create webhooks both in ghost and ck
-
-// Most important things:
-// subscribe on ck => ghost
-// unsubscibre ck => ghost
-// un/sub ghost => ck
-
 import { handleConvertkitEvent } from "./handlers/convertkit.mjs";
 import { handleGhostEvent } from "./handlers/ghost.mjs";
 
